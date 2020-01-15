31c946fe420c7d0af2147dc2b986c961e9b1f756
smackflad <ask.kalamakhs@hotmail.com>
2020-01-15 16:31:25 +0200

    start commit

--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 using namespace std;
-void bla();
-
-
-int skadudle(){
-  return 5;
-}
-int skrap(){
-  return 3;
-}
 
 int main(){
   return 0;
-}
-
-int nothing(){
-  return -1;
-}
-void bla(){
-	cout<<"skipapap";
 }
