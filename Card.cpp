9198fc63bbb90ead98bcf4071495ddeedb0e4996
smackflad <ask.kalamakhs@hotmail.com>
2020-01-18 13:39:55 +0200

    added things

--- Card.cpp
+++ Card.cpp
@@ -1,38 +1,164 @@
 #include "Card.hpp"
-
-Card::Card(string nam, int cos):
-name(nam), cost(cos){
-  isTapped=0;
+#include<iostream>
+Card::Card(string nam, int cos, bool iTap):
+name(nam), cost(cos), isTapped(iTap){
 }
 
 ////////////////////GreenCard
-GreenCard::GreenCard(string nam, int cos):
-Card(nam, cos){
-  attackBonus =0;
-  defenceBonus=0;
-  minimumHonour=0;
-  cardText ="random for now";
-  effectBonus=0;
-  effectCost=0;
-  durability=0;
+GreenCard::GreenCard(string nam, int cos, int attacB, int defencB, int minH, int effecB, int effecC, string cardT):
+Card(nam, cos, false){
+  attackBonus= attacB;
+  defenceBonus= defencB;
+  minimumHonour= minH;
+  effectBonus= effecB;
+  effectCost= effecC;
+  cardText= cardT;
 }
 
-Black::Black(string nam, int cos):
-Card(nam, cos){
+
+////////////////////Follower
+Follower::Follower(string nam, int cos, int attacB, int defencB, int minH, int effecB, int effecC, string cardT):
+GreenCard(nam, cos, attacB, defencB, minH, effecB, effecC, cardT){
+
+}
+
+////////////////////FootSoldier
+FootSoldier::FootSoldier(string nam, string cardT):
+Follower(nam, 0, 2, 0, 1, 1, 2, cardT){
+
+}
+
+////////////////////Archer
+Archer::Archer(string nam, string cardT):
+Follower(nam, 0, 0, 2, 1, 1, 2, cardT){
+
+}
+
+////////////////////Cavalry
+Cavalry::Cavalry(string nam, string cardT):
+Follower(nam, 3, 4, 2, 3, 3, 4, cardT){
+
+}
+
+////////////////////Bushido
+Bushido::Bushido(string nam, string cardT):
+Follower(nam, 8, 8, 8, 6, 3, 8, cardT){
+
+}
+
+////////////////////Sieger
+Sieger::Sieger(string nam, string cardT):
+Follower(nam, 5, 3, 3, 2, 2, 3, cardT){
+
+}
+
+////////////////////Naval
+Naval::Naval(string nam, string cardT):
+Follower(nam, 3, 2, 4, 3, 3, 4, cardT){
+
+}
+
+////////////////////Item
+
+
+
+
+
+
+////////////////////
+BlackCard::BlackCard(string nam, int cos):
+Card(nam, cos, false){
   IsRevealed=false;
+  cost=0;
+}
+
+Personalities::Personalities():
+BlackCard(){
+
   attack=0;
   defense=0;
   honor=0;
+  isDead=false;
 }
 
 Holding::Holding(string nam, int cos):
-Black(nam, cos){
-
+BlackCard(){
+  harvest=0;
 }
 
-// Stronghold::Stronghold():
-// Card(){
-//   honor=0;
-//   money=69;
-//   InitialDefense=0;
-// }
+Stronghold::Stronghold(string nam, int cos):
+BlackCard(){
+  honor=0;
+  money=69;
+  InitialDefense=0;
+}
+
+Attacker::Attacker():Personalities(){
+  cost=5;
+  attack=3;
+  defense=2;
+  honor=2;
+  isDead=false;
+}
+
+Defender::Defender():Personalities(){
+  cost=5;
+  attack=2;
+  defense=3;
+  honor=2;
+  isDead=false;
+}
+
+Shogun::Shogun():Personalities(){
+  cost=15;
+  attack=10;
+  defense=5;
+  honor=8;
+  isDead=false;
+}
+
+Chancellor::Chancellor():Personalities(){
+  cost=15;
+  attack=5;
+  defense=10;
+  honor=8;
+  isDead=false;
+}
+
+Champion::Champion():Personalities(){
+  cost=30;
+  attack=20;
+  defense=20;
+  honor=12;
+  isDead=false;
+}
+
+Solo::Solo():Holding(){
+  cost=2;
+  harvest=2;
+}
+
+Plain::Plain():Holding(){
+  cost=2;
+  harvest=2;
+}
+
+Farms::Farms():Holding(){
+  cost=3;
+  harvest=4;
+}
+
+Mine::Mine():Holding(){
+  cost=5;
+  harvest=3;
+}
+
+GoldMine::GoldMine():Holding(){
+  cost=7;
+  harvest=5;
+}
+
+CrystalMine::CrystalMine():Holding(){
+  cost=12;
+  harvest=6;
+}
--- Card.hpp
+++ Card.hpp
@@ -1,67 +1,235 @@
 #include <iostream>
 using namespace std;
 
+
+
 class Card{
-private:
+protected:
   string name;
   int cost;
   bool isTapped;
 public:
-  Card(string nam, int cos);
+  Card(string nam, int cos, bool iTap);
 };
 
-class GreenCard:public Card{
-private:
+/////////////////////////////////////////////<GreenCard>
+class GreenCard:public Card{//this should be abstruct
+protected:
   int attackBonus;
   int defenceBonus;
   int minimumHonour;
-  string cardText;
   int effectBonus;
   int effectCost;
+
+  string cardText;
+public:
+  GreenCard(string nam, int cos, int attacB, int defencB, int minH, int effecB, int effecC, string cardT);
+
+};
+
+class Follower:public GreenCard{//prosopikh froyra, this should be abstruct
+public:
+  Follower(string nam, int cos, int attacB, int defencB, int minH, int effecB, int effecC, string cardT);
+};
+
+//akolou8oi
+class FootSoldier:public Follower{
+private:
+
+public:
+  FootSoldier(string nam, string cardT):
+};
+
+class Archer:public Follower{
+private:
+
+public:
+  Archer(string nam, string cardT);
+};
+
+class Cavalry:public Follower{
+private:
+
+public:
+  Cavalry(string nam, string cardT);
+};
+
+class Bushido:public Follower{
+private:
+
+public:
+  Bushido(string nam, string cardT):
+};
+
+class Sieger:public Follower{
+private:
+
+public:
+  Sieger(string nam, string cardT):
+};
+
+class Naval:public Follower{
+private:
+
+public:
+  Naval(string nam, string cardT):
+};
+//</akolou8oi>
+
+
+class Item:public GreenCard{//eksoplismos prosopikothtas
+private:
   int durability;
 public:
-  GreenCard(string nam, int cos);
+  Item(string nam, int cos, int attacB, int defencB, int minH, int effecB, int effecC, string cardT, int dur);
+};
+
+//antikeimena
+class Katana:public Item{
 
 };
 
-class Follower:public GreenCard{//prosopikh froyra
+class Spear:public Item{
 
 };
 
-class Item:public Follower{//eksoplismos prosopikothtas
+class Bow:public Item{
 
 };
 
-class Black:public Card{
-private:
+class Ninjato:public Item{
+
+};
+
+class Wakizashi:public Item{
+
+};
+//</antikeimena>
+
+/////////////////////////////////////////////</GreenCard>
+
+/////////////////////////////////////////////<BlackCard>
+class BlackCard:public Card{
+protected:
   bool IsRevealed;
+  int cost;
+public:
+  BlackCard(string nam, int cos);
+};
+
+class Personalities:public BlackCard{
+private:
+
+public:
+  Personalities();
   int attack;
   int defense;
   int honor;
-public:
-  Black(string nam, int cos);
+  bool isDead;
+
 };
 
-class Personality:public Black{
-private:
+class Holding:public BlackCard{
 
-public:
-  Personality();
-};
-
-class Holding:public Black{
-private:
 
 public:
   Holding(string nam, int cos);
+  int harvest;
 
 };
 
-// class Stronghold:public Black{
-// private:
-//   int honor;
-//   int money;
-//   int InitialDefense;
-// public:
-//   Stronghold();
-// };
+class Stronghold:public BlackCard{
+private:
+  int honor;
+  int money;
+  int InitialDefense;
+public:
+  Stronghold(string nam, int cos);
+};
+/////////////////////////////////<Personalities>
+class Attacker:public Personalities{
+public:
+  Attacker();
+};
+
+class Defender:public Personalities{
+public:
+  Defender();
+
+};
+
+class Shogun:public Personalities{
+public:
+  Shogun();
+};
+
+class Chancellor:public Personalities{
+public:
+  Chancellor();
+};
+
+class Champion:public Personalities{
+public:
+  Champion();
+};
+
+/////////////////////////////////<Holding>
+class Solo:public Holding{
+public:
+  Solo();
+};
+
+class Plain:public Holding{
+public:
+  Plain();
+
+};
+
+
+class Farms:public Holding{
+public:
+  Farms();
+};
+
+
+class Mine:public Holding{
+public:
+  Mine();
+
+};
+
+
+class GoldMine:public Holding{
+public:
+  GoldMine();
+};
+
+
+class CrystalMine:public Holding{
+public:
+  CrystalMine();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////</BlackCard>
